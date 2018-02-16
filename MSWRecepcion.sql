102e16ea52b6bb541902f45e1e2e7f5508292ba9
giovani33 <12460735@itcolima.edu.mx>
2018-02-16 09:39:04 -0600

    modificacion de Numserie y smart a tipo null

--- MSWRecepcion.sql
+++ MSWRecepcion.sql
@@ -23,8 +23,8 @@ create table Registro_Llamadas(
 	fecha datetime not null,
 	Usuario nvarchar(100) not null,
 	Notas nvarchar(250)  null,
-	NumserieCampeon nvarchar(30)not null,
-	NumserieSmart nvarchar(30)not null,
+	NumserieCampeon nvarchar(30) null,
+	NumserieSmart nvarchar(30) null,
 	Atendio nvarchar(100) not null,
 	
 	primary key(Id_Rllamadas),
